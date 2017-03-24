600e3dcb60952238ba3a6f6253559b47fdcfa184
Sebastian Schubert <sschubert932@gmail.com>
2017-03-24 15:23:38 +0100

    Restructuring

--- src/james-bot.ts
+++ src/james-bot.ts
@@ -16,8 +16,8 @@ export class JamesBot {
     constructor(slackClientId: string, slackClientSecret: string, port: string, mittagApiKey: string) {
         this._controller = Botkit.slackbot({
             debug: true,
-            json_file_store: './db_slackbutton_bot/'
-            // interactive_replies: true
+            json_file_store: './bot_data/',
+            interactive_replies: true
         });
 
         this._controller.configureSlackApp({
@@ -50,9 +50,7 @@ export class JamesBot {
             })
         });
 
-
         this._controller.on('create_bot', (bot,config) => {
-            debugger;
             console.log(this._bots);
 
             if (this._bots[bot.config.token]) {
@@ -101,7 +99,7 @@ export class JamesBot {
             }
 
             // connect all teams with bots up to slack!
-            for (var t  in teams) {
+            for (var t in teams) {
                 if (teams[t].bot) {
                     this._controller.spawn(teams[t]).startRTM(function(err, bot) {
                         if (err) {
@@ -115,8 +113,170 @@ export class JamesBot {
         });
     }
 
-
     private initDefaultBehavior() {
+        this._controller.on('interactive_message_callback', function(bot, message) {
+            var ids = message.callback_id.split(/\-/);
+            var user_id = ids[0];
+            var item_id = ids[1];
+
+            this._controller.storage.users.get(user_id, function(err, user) {
+
+                if (!user) {
+                    user = {
+                        id: user_id,
+                        list: []
+                    }
+                }
+
+                for (var x = 0; x < user.list.length; x++) {
+                    if (user.list[x].id == item_id) {
+                        if (message.actions[0].value=='flag') {
+                            user.list[x].flagged = !user.list[x].flagged;
+                        }
+                        if (message.actions[0].value=='delete') {
+                            user.list.splice(x,1);
+                        }
+                    }
+                }
+
+
+                var reply = {
+                    text: 'Here is <@' + user_id + '>s list:',
+                    attachments: [],
+                }
+
+                for (var x = 0; x < user.list.length; x++) {
+                    reply.attachments.push({
+                        title: user.list[x].text + (user.list[x].flagged? ' *FLAGGED*' : ''),
+                        callback_id: user_id + '-' + user.list[x].id,
+                        attachment_type: 'default',
+                        actions: [
+                            {
+                                "name":"flag",
+                                "text": ":waving_black_flag: Flag",
+                                "value": "flag",
+                                "type": "button",
+                            },
+                            {
+                            "text": "Delete",
+                                "name": "delete",
+                                "value": "delete",
+                                "style": "danger",
+                                "type": "button",
+                                "confirm": {
+                                "title": "Are you sure?",
+                                "text": "This will do something!",
+                                "ok_text": "Yes",
+                                "dismiss_text": "No"
+                                }
+                            }
+                        ]
+                    })
+                }
+
+                bot.replyInteractive(message, reply);
+                this.contr
+                this._controller.storage.users.save(user);
+
+
+            });
+
+        });
+
+        this._controller.hears('interactive', 'direct_message', function(bot, message) {
+            bot.reply(message, {
+                attachments:[
+                    {
+                        title: 'Do you want to interact with my buttons?',
+                        callback_id: '123',
+                        attachment_type: 'default',
+                        actions: [
+                            {
+                                "name":"yes",
+                                "text": "Yes",
+                                "value": "yes",
+                                "type": "button",
+                            },
+                            {
+                                "name":"no",
+                                "text": "No",
+                                "value": "no",
+                                "type": "button",
+                            }
+                        ]
+                    }
+                ]
+            });
+        });
+
+        this._controller.hears(['list','tasks'],'direct_mention,direct_message',function(bot,message) {
+            this._controller.storage.users.get(message.user, function(err, user) {
+                if (!user) {
+                    user = {
+                        id: message.user,
+                        list: []
+                    }
+                }
+
+                if (!user.list || !user.list.length) {
+                    user.list = [
+                        {
+                            'id': 1,
+                            'text': 'Test Item 1'
+                        },
+                        {
+                            'id': 2,
+                            'text': 'Test Item 2'
+                        },
+                        {
+                            'id': 3,
+                            'text': 'Test Item 3'
+                        }
+                    ]
+                }
+
+                var reply = {
+                    text: 'Here is your list. Say `add <item>` to add items.',
+                    attachments: [],
+                }
+
+                for (var x = 0; x < user.list.length; x++) {
+                    reply.attachments.push({
+                        title: user.list[x].text + (user.list[x].flagged? ' *FLAGGED*' : ''),
+                        callback_id: message.user + '-' + user.list[x].id,
+                        attachment_type: 'default',
+                        actions: [
+                            {
+                                "name":"flag",
+                                "text": ":waving_black_flag: Flag",
+                                "value": "flag",
+                                "type": "button",
+                            },
+                            {
+                            "text": "Delete",
+                                "name": "delete",
+                                "value": "delete",
+                                "style": "danger",
+                                "type": "button",
+                                "confirm": {
+                                "title": "Are you sure?",
+                                "text": "This will do something!",
+                                "ok_text": "Yes",
+                                "dismiss_text": "No"
+                                }
+                            }
+                        ]
+                    })
+                }
+
+                bot.reply(message, reply);
+
+                this._controller.storage.users.save(user);
+            });
+
+        });
+
+
 
     }
 }
